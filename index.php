7245206cec9c4a2766b702fcfc3c32f689640515
HodeonArtz <martidicianolold@gmail.com>
2024-10-09 18:48:37 +0200

    closes #1 bank accounts created

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bank account</title>
+</head>
+
+<body>
+  <?php 
+    $bank_account1 = [
+      "balance"=> 400,
+      "overdraft_limit"=> 0,
+    ];
+    $bank_account2 = [
+      "balance"=> 200,
+      "overdraft_limit"=> 100,
+    ]
+    
+  ?>
+</body>
+
+</html>
